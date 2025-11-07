8ff712a388e83d09058dacdc3540f2e62d350ad8
Greedysky <greedysky@163.com>
2025-11-07 15:21:25 +0800

    feat: Support for qmmp 0.x Qt4

--- decoderorganyafactory.cpp
+++ decoderorganyafactory.cpp
@@ -93,3 +93,8 @@ QString DecoderOrganyaFactory::translation() const
 {
     return QString();
 }
+
+#if QT_VERSION < QT_VERSION_CHECK(5,0,0)
+#include <QtPlugin>
+Q_EXPORT_PLUGIN2(organya, DecoderOrganyaFactory)
+#endif
